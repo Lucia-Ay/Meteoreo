1ad9c13d0a9cf16c2455e9f6cad8b78d75bb6ae3
Lucia Aydin <114146153+Lucia-Ay@users.noreply.github.com>
2023-09-03 08:21:13 +0300

    Move files

---
+++ src/js/api.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const API_KEY = '89aad541f3a3c7d7f9925bdbab022307';
+
+/* Fetch data from server */
+
+export const fetchData = (URL, callback) => {
+  fetch(`${URL}&appid=${API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
